4fa62060b55625c68a736612d4bad864819b8dc4
Joselito Jr <motajoselito@hotmail.com>
2017-03-12 00:31:15 -0300

    Depuração de Consultas

--- app/models/apartamento.rb
+++ app/models/apartamento.rb
@@ -3,11 +3,6 @@ class Apartamento < ApplicationRecord
 	has_many :vaga
 	has_many :encomenda
 
-
-	
-
 	validates_presence_of :local, :nome_proprietario, :cpf_proprietario, :nome_morador, :senha
-
-
 
 end
